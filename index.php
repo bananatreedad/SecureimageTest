f27809d4a948d5eb63c92a1719737d7f7dabdbcf
Jonathan Meier <jnt.meier@gmail.com>
2015-02-02 22:49:51 +0100

    remove line under link

--- index.php
+++ index.php
@@ -6,8 +6,8 @@
 <body>
 	<form action="check.php" method="POST">
 	<img id="captcha" src="securimage/securimage_show.php" alt="CAPTCHA Image" /> 
-	<a href="#" onclick="document.getElementById('captcha').src = 'securimage/securimage_show.php?' + Math.random(); return false">
-		<img src="images/refresh.ico" width="15" height="15">
+	<a href="#" onclick="document.getElementById('captcha').src = 'securimage/securimage_show.php?' + Math.random(); return false" style="text-decoration: none">
+		<img src="images/refresh.ico">
 	</a>
 	<br/>
 	<input type="text" name="captcha_code" size="10" maxlength="6" /> <!-- change size and maxlength to fit your needs -->
